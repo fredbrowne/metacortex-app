2c5cb32b48355abc6e13106d2c257ac7cfdb3ae7
Fred Browne <fredericosetra@yahoo.com.br>
2021-11-05 00:48:56 -0300

    Navigation Abstraction and Signin/Signup boilerplate

--- App.js
+++ App.js
@@ -1,105 +1,78 @@
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-/* Tabs Screens */
-import HomeScreen from './src/screens/HomeScreen';
-import CalculatorScreen from './src/screens/CalculatorScreen';
-import ProfileScreen from './src/screens/ProfileScreen';
-import CalendarScreen from './src/screens/CalendarScreen';
-import MedicalScreen from './src/screens/MedicalScreen';
-/* Icons */
-import { Feather, SimpleLineIcons, FontAwesome, FontAwesome5 } from '@expo/vector-icons'
+import React from 'react';
+/* Navigation */
+import Navigation from './config/Navigation';
 
-const Tab = createBottomTabNavigator();
+export default () => <Navigation />;
 
-function MyTabs() {
-  return (
-    <Tab.Navigator
-      initialRouteName="Feed"
-      screenOptions={{
-        tabBarInactiveTintColor: '#e91e63'
-      }}
-    >
-      <Tab.Screen
-        name="Calendar"
-        component={ CalendarScreen }
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: ({ color, size }) => (
-            <Feather name="calendar" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Calculator"
-        component={ CalculatorScreen }
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: ({ color, size }) => (
-            <SimpleLineIcons name="calculator" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Home"
-        component={ HomeScreen }
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: ({ color, size }) => (
-            <Feather name="home" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Feed"
-        component={ MedicalScreen }
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome5 name="book-medical" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ ProfileScreen }
-        options={{
-          tabBarLabel: "",
-          tabBarIcon: ({ color, size }) => (
-            <FontAwesome name="user" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  )
-}
+// const Tab = createBottomTabNavigator();
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <MyTabs />
-    </NavigationContainer>
-  );
-}
+// function MyTabs() {
+//   return (
+//     <Tab.Navigator
+//       initialRouteName="Feed"
+//       screenOptions={{
+//         tabBarInactiveTintColor: '#e91e63'
+//       }}
+//     >
+//       <Tab.Screen
+//         name="Calendar"
+//         component={ CalendarScreen }
+//         options={{
+//           tabBarLabel: "",
+//           tabBarIcon: ({ color, size }) => (
+//             <Feather name="calendar" color={color} size={size} />
+//           ),
+//         }}
+//       />
+//       <Tab.Screen
+//         name="Calculator"
+//         component={ CalculatorScreen }
+//         options={{
+//           tabBarLabel: "",
+//           tabBarIcon: ({ color, size }) => (
+//             <SimpleLineIcons name="calculator" color={color} size={size} />
+//           ),
+//         }}
+//       />
+//       <Tab.Screen
+//         name="Home"
+//         component={ HomeScreen }
+//         options={{
+//           tabBarLabel: "",
+//           tabBarIcon: ({ color, size }) => (
+//             <Feather name="home" color={color} size={size} />
+//           ),
+//         }}
+//       />
+//       <Tab.Screen
+//         name="Feed"
+//         component={ MedicalScreen }
+//         options={{
+//           tabBarLabel: "",
+//           tabBarIcon: ({ color, size }) => (
+//             <FontAwesome5 name="book-medical" color={color} size={size} />
+//           ),
+//         }}
+//       />
+//       <Tab.Screen
+//         name="Profile"
+//         component={ ProfileScreen }
+//         options={{
+//           tabBarLabel: "",
+//           tabBarIcon: ({ color, size }) => (
+//             <FontAwesome name="user" color={color} size={size} />
+//           ),
+//         }}
+//       />
+//     </Tab.Navigator>
+//   )
+// }
 
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <MyTabs />
+//     </NavigationContainer>
+//   );
+// }
 
-
-
-
-
-
-
-
-
-/*
-const navigator = createStackNavigator({
-  Home: HomeScreen
-}, {
-  initialRouteName: 'Home',
-  defaultNavigationOptions: {
-    title: 'Home'
-  }
-});
-
-export default createAppContainer(navigator);
-*/
---
+++ config/Navigation.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { NavigationContainer  } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+/* Screen Import */
+import HomeScreen from '../src/screens/HomeScreen';
+import CalculatorScreen from '../src/screens/CalculatorScreen';
+import ProfileScreen from '../src/screens/ProfileScreen';
+import CalendarScreen from '../src/screens/CalendarScreen';
+import MedicalScreen from '../src/screens/MedicalScreen';
+/* Icons Import */
+import { Feather, SimpleLineIcons, FontAwesome, FontAwesome5 } from '@expo/vector-icons'
+
+/* Bottom Tab */
+const AppTabs = createBottomTabNavigator();
+const AppTabsScreen = () => (
+    <AppTabs.Navigator
+        screenOptions={{
+            tabBarActiveTintColor: '#fa8072'
+        }}>
+        <AppTabs.Screen name="Calendar" component={CalendarScreen} options={{
+          tabBarIcon: ({ color, size }) => (
+            <Feather name="calendar" color={color} size={size} />
+        )}}/>
+        <AppTabs.Screen name="Calculator" component={CalculatorScreen} options={{
+
+            tabBarIcon: ({ color, size }) => (
+                <SimpleLineIcons name="calculator" color={color} size={size} />
+          )}}/>
+          <AppTabs.Screen name="Home" component={HomeScreen} options={{
+            tabBarIcon: ({ color, size }) => (
+                <Feather name="home" color={color} size={size} />
+          )}}/>
+          <AppTabs.Screen name="Medical" component={MedicalScreen} options={{
+            tabBarIcon: ({ color, size }) => (
+                <FontAwesome5 name="book-medical" color={color} size={size} />
+          )}}/>
+          <AppTabs.Screen name="Profile" component={ProfileScreen} options={{
+            tabBarIcon: ({ color, size }) => (
+                <FontAwesome name="user" color={color} size={size} />
+          )}}/>
+    </AppTabs.Navigator>
+);
+
+export default () => (
+    <NavigationContainer>
+        <AppTabsScreen />
+    </NavigationContainer>
+);
