6318e37a387930a6265323b890183b07d6ac4229
Fred Browne <fredericosetra@yahoo.com.br>
2021-11-05 18:59:32 -0300

    Firebase Login/Signup screen

--- App.js
+++ App.js
@@ -1,5 +1,38 @@
-import React from 'react';
-/* Navigation */
+import React from 'react';4
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './src/screens/HomeScreen';
+import CalculatorScreen from './src/screens/CalculatorScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import CalendarScreen from './src/screens/CalendarScreen';
+import MedicalScreen from './src/screens/MedicalScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+/*
 import Navigation from './config/Navigation';
 
 export default () => <Navigation />;
+*/
+
+const navigator = createSwitchNavigator({
+    loginFlow: createStackNavigator({
+      Signin: SigninScreen,
+      Signup: SignupScreen
+    }),
+    mainFlow: createStackNavigator({
+        Home: HomeScreen,
+        Calculator: CalculatorScreen,
+        Profile: ProfileScreen,
+        Medical: MedicalScreen,
+        Calendar: CalendarScreen
+        },{
+        initialRouteName: "Home",
+        defaultNavigationOptions: {
+          title: 'Home',
+          headerStyle:{backgroundColor:'orange'}
+        }
+      })
+  });
+  
+  
+  export default createAppContainer(navigator);
